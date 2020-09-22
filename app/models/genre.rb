d607d640af1a95f98a1b81a4fbefcfde7947617a
fukitayujiro <hujiiryotaku@gmail.com>
2020-09-23 08:39:34 +0900

    フロント実装

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -4,6 +4,8 @@ class Genre < ActiveHash::Base
     { id: 2, name: '経営' },
     { id: 3, name: 'IT' },
     { id: 4, name: '自己啓発' },
-    { id: 5, name: 'マーケティング' }
+    { id: 5, name: 'マーケティング' },
+    { id: 6, name: '営業' },
+    { id: 7, name: 'その他' }
   ]
 end
--- app/models/medium.rb
+++ app/models/medium.rb
@@ -4,6 +4,9 @@ class Medium < ActiveHash::Base
     { id: 2, name: '書籍' },
     { id: 3, name: '動画' },
     { id: 4, name: 'Udemy' },
-    { id: 5, name: '無料ウェブサービス' }
+    { id: 5, name: 'MENTA' },
+    { id: 6, name: 'ウェブサービス(無料)' },
+    { id: 7, name: 'ウェブサービス(有料)' },
+    { id: 8, name: 'その他' },
   ]
 end
