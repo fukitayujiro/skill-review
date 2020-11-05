75e4de96eba183cca570896f5b8d05d5ef3deb61
fukitayujiro <hujiiryotaku@gmail.com>
2020-11-05 22:07:24 +0900

    Contentsテーブルから現状不要になったカラムを削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_23_085530) do
+ActiveRecord::Schema.define(version: 2020_11_05_130546) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -38,9 +38,7 @@ ActiveRecord::Schema.define(version: 2020_09_23_085530) do
     t.integer "media_id"
     t.integer "genre_id"
     t.string "introduction"
-    t.string "url"
     t.integer "user_id"
-    t.integer "judge"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
