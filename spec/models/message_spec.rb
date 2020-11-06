65d0bc47856e32a620cf5c36c9a5082558ede9c7
fukitayujiro <hujiiryotaku@gmail.com>
2020-11-06 16:07:12 +0900

    テストコード設置

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
-RSpec.describe Message, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Message do
+  before do
+    @message = FactoryBot.build(:message)
+  end
+  describe 'メッセージ投稿機能' do
+    context 'メッセージ投稿がうまくいくとき' do
+      it "項目が全て埋まっている時" do
+        expect(@message).to be_valid
+      end
+    end
+    context 'コンテンツ投稿がうまくいかない時' do
+      it "pointが洗濯してください以外の時" do
+        @message.point = "1"
+        @content.valid?
+        expect(@message.errors.full_messages).to include("Point must be other than 1")
+      end
+      it "introductionが空の時" do
+        @content.text = nil
+        @content.valid?
+        expect(@content.errors.full_messages).to include("Text can't be blank")
+      end
+    end
+  end
 end
