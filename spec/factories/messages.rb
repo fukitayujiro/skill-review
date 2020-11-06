88ca3da516ca476a04c86b4c7fe310202f889280
fukitayujiro <hujiiryotaku@gmail.com>
2020-11-06 16:12:18 +0900

    修正

--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -2,6 +2,8 @@ FactoryBot.define do
   factory :message do
     point              {"2"}
     text                {"1111112222233333"}
+    user_id              {"2"}
+    content_id           {"2"}
   end
 
 end
