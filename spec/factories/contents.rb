9883c5867da21af97add34058fcc8b49bf8cb2eb
fukitayujiro <hujiiryotaku@gmail.com>
2020-11-08 16:19:11 +0900

    アソシエーション記述

--- spec/factories/contents.rb
+++ spec/factories/contents.rb
@@ -4,5 +4,6 @@ FactoryBot.define do
     media_id                 {"2"}
     genre_id              {"2"}
     introduction        {"11111111111111122222222222222222223333333333333333344444444444444444444"}
+    association :user
   end
 end
