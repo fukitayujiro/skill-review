9238232d5a0c5360021d2daf5d24bd5aa3f4ec8e
fukitayujiro <hujiiryotaku@gmail.com>
2020-11-09 16:50:43 +0900

    Userのログイン可能時の結合テストを実装

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -23,7 +23,7 @@ RSpec.describe "Users", type: :system do
       }.to change { User.count }.by(1)
       # トップページへ遷移する
       expect(current_path).to eq root_path
-      # カーソルを合わせるとログアウトボタンとマイページボタンが表示されることを確認する
+      # マイページやログアウトボタンが表示されている
       expect(page).to have_content('マイページ')
       expect(page).to have_content('ログアウト')
       # サインアップページへ遷移するボタンや、ログインページへ遷移するボタンが表示されていないことを確認する
@@ -50,6 +50,35 @@ RSpec.describe "Users", type: :system do
       }.to change { User.count }.by(0)
       # 新規登録ページへ戻されることを確認する
       expect(current_path).to eq new_user_registration_path
+    end
+  end
+end
+
+RSpec.describe 'ログイン', type: :system do
+  before do
+    @user = FactoryBot.create(:user)
+  end
+  context 'ログインができるとき' do
+    it '保存されているユーザーの情報と合致すればログインができる' do
+      # トップページに移動する
+      visit root_path
+      # トップページにログインページへ遷移するボタンがあることを確認する
+      expect(page).to have_content('ログイン')
+      # ログインページへ遷移する
+      visit new_user_session_path
+      # 正しいユーザー情報を入力する
+      fill_in 'user[email]', with: @user.email
+      fill_in 'user[password]', with: @user.password
+      # ログインボタンを押す
+      find('input[name="commit"]').click
+      # トップページへ遷移することを確認する
+      expect(current_path).to eq root_path
+      # マイページやログアウトボタンが表示されている
+      expect(page).to have_content('マイページ')
+      expect(page).to have_content('ログアウト')
+      # サインアップページへ遷移するボタンやログインページへ遷移するボタンが表示されていないことを確認する
+      expect(page).to have_no_content('新規登録')
+      expect(page).to have_no_content('ログイン')
     end
   end
 end
