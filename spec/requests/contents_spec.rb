5b33b46b4f17fecb57b6e4cfff1368624e0af4bc
fukitayujiro <hujiiryotaku@gmail.com>
2020-11-15 13:28:33 +0900

    indexアクションの単体テストコード実装

--- spec/requests/contents_spec.rb
+++ spec/requests/contents_spec.rb
@@ -1,10 +1,30 @@
 require 'rails_helper'
+describe ContentsController, type: :request do
 
-RSpec.describe "Contents", type: :request do
-  describe "GET /contents" do
-    it "works! (now write some real specs)" do
-      get contents_index_path
-      expect(response).to have_http_status(200)
+  before do
+    @content = FactoryBot.create(:content)
+  end
+
+  describe "GET #index" do
+    it "indexアクションにリクエストすると正常にレスポンスが返ってくる" do 
+      get root_path
+      expect(response.status).to eq 200
+    end
+    it "indexアクションにリクエストするとレスポンスに投稿済みのツイートのテキストが存在する" do 
+      get root_path
+      expect(response.body).to include @content.title
     end
   end
+
+  describe "GET #show" do
+    it "showアクションにリクエストすると正常にレスポンスが返ってくる" do 
+    end
+    it "showアクションにリクエストするとレスポンスに投稿済みのツイートのテキストが存在する" do 
+    end
+    it "showアクションにリクエストするとレスポンスに投稿済みのツイートの画像URLが存在する" do 
+    end
+    it "showアクションにリクエストするとレスポンスにコメント一覧表示部分が存在する" do 
+    end
+  end 
+
 end
