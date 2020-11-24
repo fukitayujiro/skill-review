89e9f73c8ea4ca2f0e2731f23bd43ed7ef518ec8
fukitayujiro <hujiiryotaku@gmail.com>
2020-11-24 18:15:19 +0900

    kk

--- db/migrate/20201105130546_remove_judge_from_content.rb
+++
@@ -1,5 +0,0 @@
-class RemoveJudgeFromContent < ActiveRecord::Migration[6.0]
-  def change
-    remove_column :contents, :judge, :integer
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -33,37 +33,5 @@ ActiveRecord::Schema.define(version: 2020_11_05_130546) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
-  create_table "contents", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "title"
-    t.integer "media_id"
-    t.integer "genre_id"
-    t.string "introduction"
-    t.integer "user_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "messages", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.integer "point"
-    t.text "text"
-    t.integer "user_id"
-    t.integer "content_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.string "name"
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  end
-
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
 end
