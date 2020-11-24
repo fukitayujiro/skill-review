10295fa89354edd1ad46bb160f179eabb54805b7
fukitayujiro <hujiiryotaku@gmail.com>
2020-11-24 16:51:48 +0900

    エラー対応

--- db/migrate/20200905233515_create_contents.rb
+++ db/migrate/20200905233515_create_contents.rb
@@ -5,9 +5,7 @@ class CreateContents < ActiveRecord::Migration[6.0]
       t.integer :media_id
       t.integer :genre_id
       t.string :introduction
-      t.string :url
       t.references :user,  foreign_key: true
-      t.integer :judge
       t.timestamps
     end
   end
